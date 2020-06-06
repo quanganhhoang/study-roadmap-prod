356f25342f37d34dca1d2d59591c160f45a09fd5
QA Hoang <qahoang317@gmail.com>
2020-06-05 17:41:10 -0700

    Add heroku host to ALLOWED_HOST

--- manager/settings.py
+++ manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9=#@pkjcwswp-8h@^0(*dn&m+x)-eii9%5fqide7iq-e4sizc#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studyroadmap.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['studyroadmap.herokuapp.com']
 
 
 # Application definition
