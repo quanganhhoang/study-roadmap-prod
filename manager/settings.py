c2b3672dcc70bc8f6ba9f6cc0bba3def7174ea0e
QA Hoang <qahoang317@gmail.com>
2020-06-05 18:06:31 -0700

    Fix Your WhiteNoise configuration is incompatible with WhiteNoise v4.0 error

--- manager/settings.py
+++ manager/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATIC_FILES_URL = (
 #   os.path.join(BASE_DIR, 'assets'),
