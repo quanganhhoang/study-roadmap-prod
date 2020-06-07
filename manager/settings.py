8ce7261280fa8670a40a605d38df65ff617e480a
QA Hoang <qahoang317@gmail.com>
2020-06-06 23:27:32 -0700

    [prod] only display browsable api in dev

--- manager/settings.py
+++ manager/settings.py
@@ -186,6 +186,19 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #   os.path.join(BASE_DIR, 'resources'),
 # )
 
+# ====== Hide Browsable API in prod ======
+
+DEFAULT_RENDERER_CLASSES = (
+    'rest_framework.renderers.JSONRenderer',
+)
+
+if DEBUG:
+    DEFAULT_RENDERER_CLASSES = DEFAULT_RENDERER_CLASSES + (
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+
+# ====== Hide Browsable API in prod ======
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
@@ -195,6 +208,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
       'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
 }
 
 # Used to override django-rest-auth registration
