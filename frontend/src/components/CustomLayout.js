62514bbefdbf1ad24c806ba2fa581aff255eebe0
QA Hoang <qahoang317@gmail.com>
2020-06-29 22:24:03 -0700

    Remove redux-thunk

--- frontend/src/components/CustomLayout.js
+++ frontend/src/components/CustomLayout.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Row, Col, Layout, Menu, Button, Input, Dropdown } from 'antd';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import { FacebookOutlined, GoogleOutlined, TwitterOutlined, UserOutlined } from '@ant-design/icons';
 
